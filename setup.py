b7525c8def33f06a6bae595143a14879848f329e
Suhail-Ahmed-88 <rajpar.suhail.ahmed@gmail.com>
2025-05-25 00:01:11 +0800

    setup logger, exception files are added

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Wine_Quality_Project"
+AUTHOR_USER_NAME = "Suhail Ahmed"
+SRC_REPO = "mlproject"
+AUTHOR_EMAIL = "rajpar.suhail.ahmed@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for ml app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
